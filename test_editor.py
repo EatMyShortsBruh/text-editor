eb25eaa75817692fd8f40156ffcd108ba495276b
EatMyShortsBruh <eatmyshortsbrotato@gmail.com>
2025-06-24 20:52:58 -0400

    edit syntax

--- test_editor.py
+++ test_editor.py
@@ -2,7 +2,7 @@ import unittest
 import tkinter as tk
 from editor import create_main_window
 
-class TestNotepadGUI(unittest.Testcase):
+class TestNotepadGUI(unittest.TestCase):
     def test_window_title(self):
         window = create_main_window()
         self.assertEqual(window.title(), "My Notepad")
